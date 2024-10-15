19857b5e1300f5badb27c0a68bb3779b2a994802
Javier Tapia <javiertapiab96@outlook.com>
2024-10-15 08:53:58 -0500

    Spring Test Context

--- src/test/java/dev/javiertap/spring6di/Spring6DiApplicationTests.java
+++ src/test/java/dev/javiertap/spring6di/Spring6DiApplicationTests.java
@@ -1,13 +1,36 @@
 package dev.javiertap.spring6di;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import dev.javiertap.spring6di.controllers.MyController;
 
 @SpringBootTest
 class Spring6DiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Autowired
+    MyController myController;
+
+    @Test
+    void testAutowiredController() {
+        System.out.println("In testAutowiredController method");
+        System.out.println(myController.sayHello());
+    }
+
+    @Test
+    void testGetControllerFromContext() {
+        MyController controller = applicationContext.getBean(MyController.class);
+        System.out.println("In testGetControllerFromContext method");
+        System.out.println(controller.sayHello());
+    }
+
+    @Test
+    void contextLoads() {
+    }
 
 }
