f4e94db797ca981814d815f8610bef58986473e9
Javier Tapia <javiertapiab96@outlook.com>
2025-08-21 23:53:37 +0000

    Add tests for MyController and demonstrate DI in Spring context

--- src/test/java/org/javiteq/spring6di/Spring6DiApplicationTests.java
+++ src/test/java/org/javiteq/spring6di/Spring6DiApplicationTests.java
@@ -1,10 +1,30 @@
 package org.javiteq.spring6di;
 
+import org.javiteq.spring6di.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class Spring6DiApplicationTests {
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Autowired
+    MyController controller;
+
+    @Test
+    void testAutowiredController() {
+        System.out.println(controller.sayHello());
+    }
+
+    @Test
+    void testGetControllerFromContext() {
+        MyController controller = applicationContext.getBean(MyController.class);
+        System.out.println(controller.sayHello());
+    }
 
 	@Test
 	void contextLoads() {
